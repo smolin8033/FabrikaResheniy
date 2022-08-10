170b82493735cd384edac1cfd94579ccb0fc6977
smoll <smolll123@mail.ru>
2022-08-10 16:53:52 +0300

    feat!: add drf spectacular documentation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,7 +22,15 @@ INSTALLED_APPS = [
     'mailing.apps.MailingConfig',
     'customer.apps.CustomerConfig',
     'message.apps.MessageConfig',
+    # side-packages
+    'drf_spectacular',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
