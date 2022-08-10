6453e70622ed5770063e1b459946e39d8fc9818d
smoll <smolll123@mail.ru>
2022-08-10 14:27:26 +0300

    feat: create model Mailing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'mailing.apps.MailingConfig',
 ]
 
 MIDDLEWARE = [
---
+++ backend/mailing/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Mailing(models.Model):
+    start_datetime = models.DateTimeField(auto_now_add=True)
+    message_text = models.TextField()
+    # фильтр свойств клиентов, на которых должна быть произведена рассылка (код мобильного оператора, тег)
+    end_datetime = models.DateTimeField()
