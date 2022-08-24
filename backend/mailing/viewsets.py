7e96bf28044044bd87011324543c94ebee8f9c37
smoll <smolll123@mail.ru>
2022-08-24 17:35:50 +0300

    feat: method update() ostensibly works properly

--- backend/mailing/viewsets.py
+++ backend/mailing/viewsets.py
@@ -22,7 +22,7 @@ class MailingViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         mailing_serializer = serialize_and_validate_mailing(request)
 
-        filter_serializer = serialize_and_validate_filter(mailing_serializer)
+        filter_serializer = serialize_and_validate_filter(request, mailing_serializer)
 
         self.perform_create(mailing_serializer, filter_serializer=filter_serializer)
         headers = self.get_success_headers(mailing_serializer.data)
@@ -34,17 +34,55 @@ class MailingViewSet(ModelViewSet):
         check_conditions(mailing)
 
     def update(self, request, *args, **kwargs):
-        mailing_serializer = serialize_and_validate_mailing(request)
+        instance = self.get_object()
 
-        filter_serializer = serialize_and_validate_filter(mailing_serializer)
+        mailing_serializer = serialize_and_validate_mailing(request, instance)
+        filter_serializer = serialize_and_validate_filter(request, mailing_serializer, instance)
 
-        self.perform_update(mailing_serializer, filter_serializer=filter_serializer)
+        self.perform_update(
+            mailing_serializer,
+            instance=instance,
+            filter_serializer=filter_serializer
+        )
         return Response(mailing_serializer.data)
 
     def perform_update(self, serializer, *args, **kwargs):
+        """
         mailing = serializer.save(filter_field=kwargs.get('filter_serializer').save())
+        не понимаю, почему не работает
+        Правильно здесь работает? Не совсем понимаю, как должно правильно все обновляться
+        """
+        filter_serializer = kwargs.get('filter_serializer')
+        instance = kwargs.get('instance')
+        filter = instance.filter_field
 
-        check_conditions(mailing)
+        updated_filter = self.update_mailing_filter(filter, filter_serializer)
+        updated_mailing = self.update_mailing_instance(instance, serializer, updated_filter)
+
+        check_conditions(updated_mailing)
+
+    def update_mailing_filter(self, filter, filter_serializer):
+        filter.operator_code = filter_serializer.validated_data['operator_code']
+        filter.tag = filter_serializer.validated_data['tag']
+
+        filter.save()
+        return filter
+
+    def update_mailing_instance(self, instance, serializer, updated_filter):
+        instance.start_datetime = serializer.validated_data['start_datetime']
+        instance.message_text = serializer.validated_data['message_text']
+        instance.filter_field = updated_filter
+        instance.end_datetime = serializer.validated_data['end_datetime']
+
+        instance.save()
+        return instance
+
+
 """
 передать instance и validated_data и просто сохранить объект
+
+ OrderedDict([('start_datetime', datetime.datetime(2022, 8, 23, 18, 14, tzinfo=zoneinfo.ZoneInfo(key='Europe/Moscow'))), ('message_text', 'third_m
+essagefirst_messagefirst_messagefirst_message'), ('end_datetime', datetime.datetime(2022, 8, 23, 20, 45, 2, tzinfo=zoneinfo.ZoneInfo(key='Europe/Moscow')))])
+
+ OrderedDict([('operator_code', 916), ('tag', 'first_tag')])
 """
--- backend/services/serializer_validation_service.py
+++ backend/services/serializer_validation_service.py
@@ -1,17 +1,21 @@
 from mailing.serializers import MailingSerializer, MailingFilterSerializer
 
 
-def serialize_and_validate_mailing(request):
+def serialize_and_validate_mailing(request, instance=None):
     serializer = MailingSerializer(data=request.data)
+    if request.method == 'PUT':
+        serializer = MailingSerializer(instance, data=request.data)
     serializer.is_valid(raise_exception=True)
 
     return serializer
 
 
-def serialize_and_validate_filter(mailing_serializer):
+def serialize_and_validate_filter(request, mailing_serializer, instance=None):
     filtered_data = mailing_serializer.validated_data.pop('filter_field')
 
-    filter_serializer = MailingFilterSerializer(data=filtered_data)
-    filter_serializer.is_valid(raise_exception=True)
+    serializer = MailingFilterSerializer(data=filtered_data)
+    if request.method == 'PUT':
+        serializer = MailingFilterSerializer(instance, data=filtered_data)
+    serializer.is_valid(raise_exception=True)
 
-    return filter_serializer
+    return serializer
