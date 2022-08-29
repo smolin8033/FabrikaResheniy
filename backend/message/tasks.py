60e63bcb8ce975153baa612696dfa47ea586c6a0
smoll <smolll123@mail.ru>
2022-08-29 17:50:08 +0300

    fix: celery task does't work properly yet

--- backend/message/tasks.py
+++ backend/message/tasks.py
@@ -12,13 +12,13 @@ def send_messages(messages_ids):
     """
     Отправка сообщений
     """
-    token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTE1MDI5MjIsImlzcyI\
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTE1MDI5MjIsImlzcyI\
     6ImZhYnJpcXVlIiwibmFtZSI6ImFsZXhzbTBsIn0.2XqlRn3_YsKz4w3EoBs_mQ5eY3ow7M3a4UqHKVGSxFo'
     headers = {
-        'Authorization': 'JWT {}'.format(token)
+        'Authorization': '{}'.format(token)
     }
     for message_id in messages_ids:
-        service_url = f'https://probe.fbrq.cloud/v1/send/{message_id}/'
+        service_url = f'https://probe.fbrq.cloud/v1/send/{message_id}'
         response = requests.post(service_url, data={'msgId': message_id}, headers=headers)
         print(response)
         print('\n')
