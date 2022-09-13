6fcc1d9a448efdfba4ecf466d77060a52298871e
smoll <smolll123@mail.ru>
2022-09-13 19:13:53 +0300

    fix: pytest isn't set up yet, needs fixing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,12 @@
+import pytest
+
+from rest_framework.test import APIClient
 from tests.tests_customers.factories.customer_factory import CustomerFactory
 from pytest_factoryboy import register
 
 register(CustomerFactory)
+
+
+@pytest.fixture
+def api_client() -> APIClient:
+    return APIClient()
--- backend/tests/tests_customers/test_customers_viewset.py
+++ backend/tests/tests_customers/test_customers_viewset.py
@@ -1,5 +1,8 @@
 import pytest
-from tests.tests_customers.factories.customer_factory import CustomerFactory
+from django.urls import reverse
+from rest_framework import status
+
+from tests.conftest import api_client
 
 
 class TestCustomerViewSet:
@@ -9,6 +12,10 @@ class TestCustomerViewSet:
 
     @pytest.mark.django_db
     def test_action_create_no_operator_code(self, customer_factory):
+        data = {"phone_number": "Номер телефона", "tag": "Тег"}
+        url = reverse("customers_list")
+        response = api_client.post(url, data=data)
+        assert response.status_code == status.HTTP_201_CREATED
 
-        print(customer_factory.phone_number)
-        assert True
+        # print(customer_factory.phone_number)
+        # assert True
